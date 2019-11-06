342a881f2ae39ce4d9cb36032afcd7409776ddb7
Pieter <pieter_versavel@yahoo.com>
2019-11-06 16:13:32 +0000

    Larger critic and actor network, able to train albeit slowly

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -4,18 +4,19 @@ import copy
 from collections import namedtuple, deque
 
 from model import Actor, Critic
+from replay_memory import ReplayMemory
 
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
+BUFFER_SIZE = 20000 #int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 3e-4        # learning rate of the critic
-WEIGHT_DECAY = 0.0001   # L2 weight decay
+WEIGHT_DECAY = 0.00001   # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -36,33 +37,40 @@ class Agent():
         self.seed = random.seed(random_seed)
 
         # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_local = Actor(state_size, action_size, random_seed, fc_units=256).to(device)
+        self.actor_target = Actor(state_size, action_size, random_seed, fc_units=256).to(device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
         # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_local = Critic(state_size, action_size, random_seed, \
+                                   fcs1_units=256*2, fc2_units=256*2, fc3_units=128*2).to(device)
+        self.critic_target = Critic(state_size, action_size, random_seed, \
+                                   fcs1_units=256*2, fc2_units=256*2, fc3_units=128*2).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        #self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = ReplayMemory(action_size, BUFFER_SIZE, BATCH_SIZE)
+        
     
     def step(self, state, action, reward, next_state, done):
-        """Save experience in replay memory, and use random sample from buffer to learn."""
-        # Save experience / reward
+        """Save experience in replay memory, and
+           use random sample from buffer to learn."""
+        
+        # Save experience to the replay buffer
         self.memory.add(state, action, reward, next_state, done)
 
-        # Learn, if enough samples are available in memory
+        # Learn if enough samples are available in the replay buffer
         if len(self.memory) > BATCH_SIZE:
             experiences = self.memory.sample()
             self.learn(experiences, GAMMA)
 
     def act(self, state, add_noise=True):
-        """Returns actions for given state as per current policy."""
+        """Return action for given state using current policy."""
+        
         state = torch.from_numpy(state).float().to(device)
         self.actor_local.eval()
         with torch.no_grad():
@@ -77,7 +85,9 @@ class Agent():
 
     def learn(self, experiences, gamma):
         """Update policy and value parameters using given batch of experience tuples.
+        
         Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        
         where:
             actor_target(state) -> action
             critic_target(state, action) -> Q-value
@@ -87,26 +97,33 @@ class Agent():
             experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
             gamma (float): discount factor
         """
+        
         states, actions, rewards, next_states, dones = experiences
 
         # ---------------------------- update critic ---------------------------- #
+        
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
+        
         # Compute Q targets for current states (y_i)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
+        
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
+        
         # Compute actor loss
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
+        
         # Minimize the loss
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
@@ -126,6 +143,7 @@ class Agent():
             target_model: PyTorch model (weights will be copied to)
             tau (float): interpolation parameter 
         """
+        
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
@@ -151,6 +169,7 @@ class OUNoise:
         self.state = x + dx
         return self.state
 
+##### No longer used #####
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
